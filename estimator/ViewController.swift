35152eed4f7c95caaf5c5cc126f82953d175eb4d
Sam Zhang <zs@juweitang.net>
2018-11-25 13:00:16 +0800

    Initial Commit

---
+++ estimator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  estimator
+//
+//  Created by Sam Zhang on 2018/11/25.
+//  Copyright © 2018 Sam Zhang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
